e21fd2715370ac243d8316abaf7f2603a355bd55
vili nguyen <vili2002@centrum.cz>
2020-04-21 18:05:02 +0200

    zalohovani service-list

--- c4pe/settings.py
+++ c4pe/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '46lp@6i@txgnn1%so8ewan!0fq8auc*cgo$=avl3k7bz(ju!xa'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '192.168.0.127']
 
 
 # Application definition
