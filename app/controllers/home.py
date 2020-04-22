41a45bd917876e83e2b996bf8de6320093c9beaa
vili nguyen <vili2002@centrum.cz>
2020-04-22 16:05:52 +0200

    z nejakeho duvodu je na strance index vetsi navbar nez kdekoliv jinde

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -7,4 +7,4 @@ def example(request):
     return render(request, 'example.html')
 
 def index(request):
-    return render(request, 'layouts/layout.html')
+    return render(request, 'home/index.html')
