5e453424788b09d0766ecfafe51c37c616714c6f
vili nguyen <vili2002@centrum.cz>
2020-04-29 00:45:17 +0200

    hveznicky se vybarvuji

--- app/static/js/reviews/index.js
+++ app/static/js/reviews/index.js
@@ -1,33 +1,36 @@
 var stars = ['oneStar', 'twoStar', 'threeStar', 'fourStar', 'fiveStar'];
+var freezed = false;
 
 function star(element) {
-    var rating = 0;
-    for(var i = 0; i < stars.length; i++)
-    {
-        document.getElementById(stars[i]).classList = "fa fa-star-o";
+    if(!freezed) {
+        var rating = 0;
+        for (var i = 0; i < stars.length; i++) {
+            document.getElementById(stars[i]).classList = "fa fa-star-o";
+        }
+        if (element.id == 'oneStar') {
+            rating = 1;
+        } else if (element.id == 'twoStar') {
+            rating = 2;
+        } else if (element.id == 'threeStar') {
+            rating = 3;
+        } else if (element.id == 'fourStar') {
+            rating = 4;
+        } else if (element.id == 'fiveStar') {
+            rating = 5;
+        }
+        for (var i = 0; i < rating; i++) {
+            document.getElementById(stars[i]).classList = "fa fa-star";
+        }
     }
-    if(element.id == 'oneStar')
+}
+function save()
+{
+    if(!freezed)
     {
-        rating = 1;
+        freezed = true;
     }
-    else if(element.id == 'twoStar')
+    else
     {
-        rating = 2;
-    }
-    else if(element.id == 'threeStar')
-    {
-        rating = 3;
-    }
-    else if(element.id == 'fourStar')
-    {
-        rating = 4;
-    }
-    else if(element.id == 'fiveStar')
-    {
-        rating = 5;
-    }
-    for(var i = 0; i < rating; i++)
-    {
-        document.getElementById(stars[i]).classList = "fa fa-star";
+        freezed = false;
     }
 }
