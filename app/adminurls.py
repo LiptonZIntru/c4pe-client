e3d9c19033cf9c212e62fe91bb5f33d864dc8422
vili nguyen <vili2002@centrum.cz>
2020-05-06 21:43:05 +0200

    zakladni struktura

--- app/adminurls.py
+++ app/adminurls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .controllers.admin import home, places, users, placetypes
 
 urlpatterns = [
+    path('', home.index, name='admin index'),
     path('places/', places.index, name='admin places'),
     path('placetypes/', placetypes.index, name='admin placetypes'),
     path('users/', users.index, name='admin users'),
--- app/controllers/admin/home.py
+++ app/controllers/admin/home.py
@@ -4,3 +4,7 @@ from django.views.defaults import page_not_found
 import json
 import requests
 from app.controllers.auth import get_user, authorized
+
+
+def index(request):
+    return render(request, 'admin/home/index.html')
