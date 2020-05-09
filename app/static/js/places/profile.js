b1b829b0ab271aa6d4cc93b14802b1d67bf22c3c
Skrymer123 <vojtechtlamka@seznam.cz>
2020-05-10 01:56:29 +0200

    prehlednost kodu /places/profile.html, reviews/index.html

--- app/static/js/places/profile.js
+++ app/static/js/places/profile.js
@@ -1,78 +1,102 @@
-$(function () {
-    for(var i = 0; i < 7; i++)
-    {
-        var e = document.getElementById('time' + i);
-        if(!e.innerHTML.includes('-'))
+var stars = ['oneStar', 'twoStar', 'threeStar', 'fourStar', 'fiveStar'];
+var freezed = false;
+var rating = 0;
+
+/* ZOBRAZÍ "NO DATA" OTEVÍRACÍ DOBU PRO NEDOSTUPNÉ ČASY */
+    $(function () {
+        for(var i = 0; i < 7; i++)
         {
-            e.innerHTML = 'no data';
+            var e = document.getElementById('time' + i);
+            if(!e.innerHTML.includes('-'))
+            {
+                e.innerHTML = 'no data';
+            }
+        }
+    });
+
+/* ZOBRAZÍ/SCHOVÁ "NEW REVIEW" BUTTON, POKUD UŽIVATEL NENÍ OPRÁVNĚNÝ */
+    $(document).ready(function(){
+        var user_id = document.getElementById('user_id').value;
+        var place_id = document.getElementById('page_id').value;
+        var isCurrent = false;
+        $.get( "/places/" + place_id + '/reviews/type/1', function ( data ) {
+            var reviews = JSON.parse(data);
+            reviews.forEach(review => {
+                if (review.user.id == user_id)
+                {
+                    isCurrent = true;
+                }
+            })
+
+            if(!isCurrent)
+            {
+                document.getElementById('newReviewButton').hidden = false;
+            }
+        })
+    })
+
+/* VYKRESLUJE HVĚZDY PŘI PSANÍ NOVÉ RECENZE */
+    function colorStars(element) {
+        if(!freezed) {
+            for (var i = 0; i < stars.length; i++) {
+                document.getElementById(stars[i]).classList = "fa fa-star-o";
+            }
+            if (element.id == 'oneStar') {
+                rating = 1;
+            } else if (element.id == 'twoStar') {
+                rating = 2;
+            } else if (element.id == 'threeStar') {
+                rating = 3;
+            } else if (element.id == 'fourStar') {
+                rating = 4;
+            } else if (element.id == 'fiveStar') {
+                rating = 5;
+            }
+            for (var i = 0; i < rating; i++) {
+                document.getElementById(stars[i]).classList = "fa fa-star";
+            }
         }
     }
-});
 
-function update(type) {
-    document.getElementById('all-link').classList = "nav-link text-primary";
-    document.getElementById('positive-link').classList = "nav-link text-primary";
-    document.getElementById('negative-link').classList = "nav-link text-primary";
-    document.getElementById('verified-link').classList = "nav-link text-primary";
-    var place_id = document.getElementById('page_id').value;
-
-    if(type == 1)
+/* ULOŽÍ ZVOLENÝ POČET HVĚZD PŘI KLIKU */
+    function saveRating()
     {
-        document.getElementById('all-link').classList = "nav-link active";
-    }
-    else if(type == 2)
-    {
-        document.getElementById('positive-link').classList = "nav-link active";
-    }
-    else if(type == 3)
-    {
-        document.getElementById('negative-link').classList = "nav-link active";
-    }
-    else if(type == 4)
-    {
-        document.getElementById('verified-link').classList = "nav-link active";
+        if(!freezed)
+        {
+            document.getElementById('rating').value = rating;
+            freezed = true;
+        }
+        else
+        {
+            freezed = false;
+        }
     }
 
-    $.get( "/places/" + place_id + "/reviews/type/" + type, function( data ) {
-        var content = "";
-        var reviews = JSON.parse(data);
-        reviews.forEach(review => {
-            var rating = "";
-            var user_icon = '';
-            var review_time = review.time.slice(5,7) + "." + review.time.slice(8,10) + "." + review.time.slice(0,4) + ' ' +
-                review.time.slice(11,16);
-            if(review.user.isVerified == 1)
-            {
-                user_icon = '<i class="fas fa-check-circle" style="color:green; padding-top: 1.4%" data-toggle="tooltip" data-placement="bottom" title="User is verified"></i> '
-            }
-            for(var i = 0; i < review.rating; i++)
-            {
-                rating += '<i class="fa fa-star"></i> ';
-            }
-            for(var i = 0; i < 5 - review.rating; i++)
-            {
-                rating += '<i class="fa fa-star-o"></i> ';
-            }
-            content += '<div class="card m-2 mt-1 mb-3">' +
-                '<div class="card-body pb-2 pt-2">' +
-                '<small class="float-right pt-2">' +
-                rating +
-                '</small>' +
-                '<p class="text-nowrap mb-2">User ' +
-                user_icon +
-                '<a href="/users/' + review.user.id + '" class="text-dark"><b>' + review.user.username + '</b></a>' +
-                ' says:' +
-                '</p>' +
-                '<div class="card p-2 pl-3 text-justify" id="reviewContent">' +
-                review.text +
-                '</div>' +
-                '<p class="text-nowrap m-0 pt-1 float-right" style="font-size: smaller">' +
-                'Review added ' + review_time +
-                '</p>' +
-                '</div>' +
-                '</div>'
+/* PSANÍ RECENZE */
+    function updateText() {
+        document.getElementById('newReviewText').value = document.getElementById('newReviewTextArea').value;
 
-        });
-        $( "#card-content" ).html( content );
-    });
-}
+    }
+
+/* COLLAPSING */
+    $("#newReviewForm").on('show.bs.collapse', function(){
+        document.getElementById('buttonCollapse').innerText = "Close your review";
+      });
+    $("#newReviewForm").on('hide.bs.collapse', function(){
+        document.getElementById('buttonCollapse').innerText = "Write a review!";
+      });
+
+/* ZAMODRÁVÁNÍ LIKE/DISLIKE BUTTONU */
+    function blueThis(id)
+    {
+        document.getElementById(id).classList.toggle("text-primary");
+        document.getElementById(id).classList.toggle("text-secondary");
+    }
+
+/* LIKE/DISLIKE RECENZE */
+    function likeReview(userID, placeID, reviewID) {
+
+    }
+    function dislikeReview(userID, placeID, reviewID) {
+
+    }
--- app/static/js/reviews/index.js
+++ app/static/js/reviews/index.js
@@ -1,41 +1,82 @@
-var stars = ['oneStar', 'twoStar', 'threeStar', 'fourStar', 'fiveStar'];
-var freezed = false;
-var rating = 0;
+/* FILTR RECENZÍ (navigation bar) */
+    function update(type) {
+        document.getElementById('all-link').classList = "nav-link text-primary";
+        document.getElementById('positive-link').classList = "nav-link text-primary";
+        document.getElementById('negative-link').classList = "nav-link text-primary";
+        document.getElementById('verified-link').classList = "nav-link text-primary";
+        var place_id = document.getElementById('page_id').value;
 
-function star(element) {
-    if(!freezed) {
-        for (var i = 0; i < stars.length; i++) {
-            document.getElementById(stars[i]).classList = "fa fa-star-o";
+        if(type == 1)
+        {
+            document.getElementById('all-link').classList = "nav-link active";
         }
-        if (element.id == 'oneStar') {
-            rating = 1;
-        } else if (element.id == 'twoStar') {
-            rating = 2;
-        } else if (element.id == 'threeStar') {
-            rating = 3;
-        } else if (element.id == 'fourStar') {
-            rating = 4;
-        } else if (element.id == 'fiveStar') {
-            rating = 5;
+        else if(type == 2)
+        {
+            document.getElementById('positive-link').classList = "nav-link active";
         }
-        for (var i = 0; i < rating; i++) {
-            document.getElementById(stars[i]).classList = "fa fa-star";
+        else if(type == 3)
+        {
+            document.getElementById('negative-link').classList = "nav-link active";
         }
-    }
-}
-function save()
-{
-    if(!freezed)
-    {
-        document.getElementById('rating').value = rating;
-        freezed = true;
-    }
-    else
-    {
-        freezed = false;
-    }
-}
-function updateText() {
-    document.getElementById('text').value = document.getElementById('newReview').value;
+        else if(type == 4)
+        {
+            document.getElementById('verified-link').classList = "nav-link active";
+        }
 
-}
+        $.get( "/places/" + place_id + "/reviews/type/" + type, function( data ) {
+            var content = "";
+            var reviews = JSON.parse(data);
+            reviews.forEach(review => {
+                var rating = "";
+                var user_icon = '';
+                var review_time = review.time.slice(5,7) + "." + review.time.slice(8,10) + "." + review.time.slice(0,4) + ' ' +
+                    review.time.slice(11,16);
+                if(review.user.isVerified == 1)
+                {
+                    user_icon = '<i class="fas fa-check-circle" style="color:green; padding-top: 1.4%" data-toggle="tooltip" data-placement="bottom" title="User is verified"></i> '
+                }
+                for(var i = 0; i < review.rating; i++)
+                {
+                    rating += '<i class="fa fa-star"></i> ';
+                }
+                for(var i = 0; i < 5 - review.rating; i++)
+                {
+                    rating += '<i class="fa fa-star-o"></i> ';
+                }
+                content += '<div class="card m-2 mt-1 mb-3">' +
+                    '<div class="card-body pb-2 pt-2">' +
+                    '<small class="float-right pt-2">' +
+                    rating +
+                    '</small>' +
+                    '<p class="text-nowrap mb-2">User ' +
+                    user_icon +
+                    '<a href="/users/' + review.user.id + '" class="text-dark"><b>' + review.user.username + '</b></a>' +
+                    ' says:' +
+                    '</p>' +
+                    '<div class="card p-2 pl-3 text-justify" id="review-Content">' +
+                    review.text +
+                    '</div>' +
+                    '<div class="row justify-content-between mb-0">' +
+                    '<div class="col-auto mt-1 ml-2 row text-nowrap">' +
+                    '<x class="defaultCursor">' +
+                    review.positiveReactions +
+                    '</x>' +
+                    '<i class="fa fa-thumbs-up px-1 pt-1 text-secondary pointer" id="negativeReaction_' + review.id +
+                    '" onmouseover="blueThis(this.id);" onmouseout="blueThis(this.id);" onclick="likeReview(' + currentUser.id + ', ' + place_id + ', ' + review.id + ');"></i>' +
+                    '<y class="defaultCursor">' + review.negativeReactions + '</y>' +
+                    '<i class="fa fa-thumbs-down pl-1 pt-1 text-secondary pointer" id="positiveReaction_' + review.id +
+                    '" onmouseover="blueThis(this.id);" onmouseout="blueThis(this.id);" onclick="dislikeReview(' + currentUser.id + ', ' + place_id + ', ' + review.id + ');"></i>' +
+                    '</div>' +
+                    '<div class="col-auto">' +
+                    '<p class="text-nowrap m-0 pt-1 float-right" style="font-size: smaller">' +
+                    'Review added ' + review_time +
+                    '</p>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>'
+
+            });
+            $( "#userReviews" ).html( content );
+        });
+    }
