6f81aa09d79d0b5e0313f18863ec71ee6223e059
vili nguyen <vili2002@centrum.cz>
2020-05-17 23:33:48 +0200

    bug 18 vyresen

---
+++ app/controllers/filters.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .auth import get_user, authorized
+from django.contrib import messages
+from django.views.decorators.http import require_http_methods
+import requests
+import json
+from datetime import datetime
+
+
+def filter(request):
+    pass
