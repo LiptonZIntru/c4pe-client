f5ac6dfb78376fe9b10d6de3eb329f1b309ec2f5
vili nguyen <vili2002@centrum.cz>
2020-05-18 02:07:33 +0200

    owners delete + create

--- app/controllers/owners.py
+++ app/controllers/owners.py
@@ -16,6 +16,41 @@ def index(request, place_id):
         owners.append(json.loads(requests.get('http://77.244.251.110/api/users/' + str(i['userID'])).text))
     return render(request, 'owners/index.html',
                   {
+                      'place': place,
                       'owners': owners,
                       'currentUser': get_user(request),
                   })
+
+
+@require_http_methods(['POST'])
+def create(request, place_id):
+    username = request.POST.get('username')
+    headers = {
+        'content-type': 'application/json',
+        'Authorization': 'Bearer ' + request.COOKIES['token']
+    }
+    data = {
+        'username': username
+    }
+    response = requests.post('http://77.244.251.110/api/places/' + place_id + '/owner', data=data, headers=headers)
+
+    if response.status_code == 200:
+        messages.success(request, 'Owner added')
+    else:
+        messages.error(request, response)
+    return redirect('owners')
+
+
+@require_http_methods(['POST'])
+def delete(request, place_id, user_id):
+    headers = {
+        'content-type': 'application/json',
+        'Authorization': 'Bearer ' + request.COOKIES['token']
+    }
+    response = requests.delete('http://77.244.251.110/api/places/' + place_id + '/owner/' + user_id, headers=headers)
+
+    if response.status_code == 200:
+        messages.success(request, 'Owner removed')
+    else:
+        messages.error(request, response)
+    return redirect('owners')
