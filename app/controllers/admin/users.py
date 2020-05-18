5e691c79823cd6d42d5e4b9884c4fedb1109d1d5
vili nguyen <vili2002@centrum.cz>
2020-05-19 00:57:16 +0200

    admin users - backend hotovo

--- app/controllers/admin/users.py
+++ app/controllers/admin/users.py
@@ -21,14 +21,50 @@ def index(request):
 
 
 def edit(request, id):
-    # TODO: user info
-    # TODO: user reviews
+    if request.method == 'GET':
+        user = json.loads(requests.get('http://77.244.251.110/api/users/' + id).text)
+        userReviews = json.loads(requests.get('http://77.244.251.110/api/users/' + id + '/reviews').text)
+        return render(request, 'admin/users/edit.html',
+                      {
+                          'user': user,
+                          'userReviews': userReviews,
+                          'currentUser': get_user(request)
+                      })
+    elif request.method == 'POST':
+        headers = {
+            'content-type': 'application/json',
+            'Authorization': 'Bearer ' + request.COOKIES['token']
+        }
+        data = {
+            "firstName": request.POST.get("firstName"),
+            "lastName": request.POST.get("lastName"),
+            "description": request.POST.get("description"),
+            "street": request.POST.get("street"),
+            "city": request.POST.get("city"),
+            "zipCode": request.POST.get("zipCode"),
+            "country": request.POST.get("country")
+        }
+        response = requests.put('http://77.244.251.110/api/users/' + id, data=json.dumps(data), headers=headers)
+        if response.status_code == 204:
+            messages.success(request, 'User profile updated')
+            return redirect('admin users')
+        else:
+            messages.error(request, 'Unknown error. Please try again')
+            return redirect('admin users edit')
     return
 
 
 def delete(request, id):
-    # TODO: delete user
-    return
+    headers = {
+        'content-type': 'application/json',
+        'Authorization': 'Bearer ' + request.COOKIES['token']
+    }
+    response = requests.delete('http://77.244.251.110/api/users/' + id, headers=headers)
+    if response.status_code == 204:
+        messages.success(request, 'User deleted')
+    else:
+        messages.error(request, 'Unknown error. Please try again')
+    return redirect('admin users')
 
 
 def delete_avatar(request, id):
