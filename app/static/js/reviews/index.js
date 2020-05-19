130d3e5fbdead63ba4e693d8423ae1ed34b498d2
Skrymer123 <vojtechtlamka@seznam.cz>
2020-05-19 22:04:58 +0200

    updatejavascriptu pro filtraci recenzi

--- app/static/js/reviews/index.js
+++ app/static/js/reviews/index.js
@@ -28,57 +28,143 @@
             var content = "";
             var reviews = JSON.parse(data);
             reviews.forEach(review => {
-                var rating = "";
-                var user_icon = '';
-                var review_time = review.time.slice(5,7) + "." + review.time.slice(8,10) + "." + review.time.slice(0,4) + ' ' +
-                    review.time.slice(11,16);
-                if(review.user.isVerified == 1)
+                if(review.user.id == user_id)
                 {
-                    user_icon = '<i class="fas fa-check-circle" style="color:green; padding-top: 1.4%" data-toggle="tooltip" data-placement="bottom" title="User is verified"></i> '
-                }
-                for(var i = 0; i < review.rating; i++)
-                {
-                    rating += '<i class="fa fa-star"></i> ';
-                }
-                for(var i = 0; i < 5 - review.rating; i++)
-                {
-                    rating += '<i class="fa fa-star-o"></i> ';
-                }
-                content += '<div class="card m-2 mt-1 mb-3">' +
-                    '<div class="card-body pb-2 pt-2">' +
-                    '<small class="float-right pt-2">' +
-                    rating +
-                    '</small>' +
-                    '<p class="text-nowrap mb-2">User ' +
-                    user_icon +
-                    '<a href="/users/' + review.user.id + '" class="text-dark"><b>' + review.user.username + '</b></a>' +
-                    ' says:' +
-                    '</p>' +
-                    '<div class="card p-2 pl-3 text-justify text-md-smaller">' +
-                    review.text +
-                    '</div>' +
-                    '<div class="row justify-content-between mb-0">' +
-                    '<div class="col-auto mt-1 ml-2 row text-nowrap">' +
-                    '<x class="defaultCursor">' +
-                    review.positiveReactions +
-                    '</x>' +
-                    '<i class="fa fa-thumbs-up px-1 pt-1 text-secondary pointer" id="negativeReaction_' + review.id +
-                    '" onmouseover="blueThis(this.id);" onmouseout="blueThis(this.id);" onclick="likeReview(' + user_id + ', ' + place_id + ', ' + review.id + ');"></i>' +
-                    '<y class="defaultCursor">' +
-                    review.negativeReactions +
-                    '</y>' +
-                    '<i class="fa fa-thumbs-down pl-1 pt-1 text-secondary pointer" id="positiveReaction_' + review.id +
-                    '" onmouseover="blueThis(this.id);" onmouseout="blueThis(this.id);" onclick="dislikeReview(' + user_id + ', ' + place_id + ', ' + review.id + ');"></i>' +
-                    '</div>' +
-                    '<div class="col-auto">' +
-                    '<p class="text-nowrap m-0 pt-1 float-right" style="font-size: smaller">' +
-                    'Review added ' + review_time +
-                    '</p>' +
-                    '</div>' +
-                    '</div>' +
-                    '</div>' +
-                    '</div>'
+                    var rating = "";
+                    var user_icon = '';
+                    var review_time = review.time.slice(5,7) + "." + review.time.slice(8,10) + "." + review.time.slice(0,4) + ' ' +
+                        review.time.slice(11,16);
+                    if(review.user.isVerified == 1)
+                    {
+                        user_icon = '<i class="fas fa-check-circle" style="color:green; padding-top: 1.4%" data-toggle="tooltip" data-placement="bottom" title="User is verified"></i> '
+                    }
+                    for(var i = 0; i < review.rating; i++)
+                    {
+                        rating += '<i class="fa fa-star"></i> ';
+                    }
+                    for(var i = 0; i < 5 - review.rating; i++)
+                    {
+                        rating += '<i class="fa fa-star-o"></i> ';
+                    }
+                    content += '<div class="card m-2 mt-1 mb-3">' +
+                        '<div class="card-body pb-2 pt-2">' +
+                        '<small class="float-right pt-2" id="staticRating">' +
+                        rating +
+                        '</small>' +
+                        '<small class="float-right pt-2" id="editReviewRating" hidden>' +
+                        '<div class="p-0 m-0 defaultCursor" onclick="saveRating(\'editRating\');">' +
+                        '<i id="oneStarEdit" class="fa fa-star" onmouseover="colorStars(this);"></i>\n' +
+                        '<i id="twoStarEdit" class="fa fa-star-o" onmouseover="colorStars(this);"></i>\n' +
+                        '<i id="threeStarEdit" class="fa fa-star-o" onmouseover="colorStars(this);"></i>\n' +
+                        '<i id="fourStarEdit" class="fa fa-star-o" onmouseover="colorStars(this);"></i>\n' +
+                        '<i id="fiveStarEdit" class="fa fa-star-o" onmouseover="colorStars(this);"></i>\n' +
+                        '</div>' +
+                        '</small>' +
+                        '<div class="row">' +
+                        '<p class="text-nowrap mb-2 pt-1 mr-2">User ' +
+                        user_icon +
+                        '<a href="/users/' + review.user.id + '" class="text-dark"><b>' + review.user.username + '</b></a>' +
+                        ' says:' +
+                        '</p>' +
+                        '<button type="button" class="btn btn-outline-secondary py-1 px-2 mb-2" onclick="editReview(' +
+                        review.rating + ');" id="editReviewButton">' +
+                        'Edit </button> <button type="submit" class="btn btn-outline-secondary py-1 px-2 mb-2" form="editReview" onclick="editReview(0);" id="submitReviewButton" hidden>' +
+                        'Save' +
+                        '</button>' +
+                        '</div>' +
+                        '<label for="editReviewTextArea" hidden></label>' +
+                        '<form id="editReview" method="post" action="/places/' + place_id + '/reviews/' + review.id + '/edit/" hidden>' +
 
+
+                        /* CSRF TOKEN HERE */
+
+
+                        '<input type="number" id="editRating" name="editRating" value="' + review.rating + '" hidden>' +
+                        '<input type="text" name="editReviewText" id="editReviewText" value="' + review.text + '" hidden>' +
+                        '<textarea onchange="updateText(\'editReviewText\', this.id);" name="editReviewTextArea" class="card p-2 pl-3 text-justify text-md-smaller disabled w-100" id="editReviewTextArea" rows="3" style="min-height: 2.7em; max-height: 27em;">' +
+                        review.text + '</textarea>' +
+                        '</form>' +
+                        '<div class="card p-2 pl-3 text-justify" id="editReviewContent">' +
+                        review.text +
+                        '</div>' +
+                        '<div class="row justify-content-between mb-0">' +
+                        '<div class="col-auto mt-1 ml-2 row text-nowrap">' +
+                        '<x class="defaultCursor">' +
+                        review.positiveReactions +
+                        '</x>' +
+                        '<i class="fa fa-thumbs-up px-1 pt-1 text-secondary pointer" id="negativeReaction_' + review.id +
+                        '" onmouseover="blueThis(this.id);" onmouseout="blueThis(this.id);" onclick="likeReview(' + user_id + ', ' + place_id + ', ' + review.id + ');"></i>' +
+                        '<y class="defaultCursor">' +
+                        review.negativeReactions +
+                        '</y>' +
+                        '<i class="fa fa-thumbs-down pl-1 pt-1 text-secondary pointer" id="positiveReaction_' + review.id +
+                        '" onmouseover="blueThis(this.id);" onmouseout="blueThis(this.id);" onclick="dislikeReview(' + user_id + ', ' + place_id + ', ' + review.id + ');"></i>' +
+                        '</div>' +
+                        '<div class="col-auto">' +
+                        '<p class="text-nowrap m-0 pt-1 float-right" style="font-size: smaller">' +
+                        'Review added ' + review_time +
+                        '</p>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>'
+                }
+            });
+
+            reviews.forEach(review => {
+                if(review.user.id != user_id)
+                {
+                    var rating = "";
+                    var user_icon = '';
+                    var review_time = review.time.slice(5,7) + "." + review.time.slice(8,10) + "." + review.time.slice(0,4) + ' ' +
+                        review.time.slice(11,16);
+                    if(review.user.isVerified == 1)
+                    {
+                        user_icon = '<i class="fas fa-check-circle" style="color:green; padding-top: 1.4%" data-toggle="tooltip" data-placement="bottom" title="User is verified"></i> '
+                    }
+                    for(var i = 0; i < review.rating; i++)
+                    {
+                        rating += '<i class="fa fa-star"></i> ';
+                    }
+                    for(var i = 0; i < 5 - review.rating; i++)
+                    {
+                        rating += '<i class="fa fa-star-o"></i> ';
+                    }
+                    content += '<div class="card m-2 mt-1 mb-3">' +
+                        '<div class="card-body pb-2 pt-2">' +
+                        '<small class="float-right pt-2">' +
+                        rating +
+                        '</small>' +
+                        '<p class="text-nowrap mb-2">User ' +
+                        user_icon +
+                        '<a href="/users/' + review.user.id + '" class="text-dark"><b>' + review.user.username + '</b></a>' +
+                        ' says:' +
+                        '</p>' +
+                        '<div class="card p-2 pl-3 text-justify text-md-smaller" id="reviewContent">' +
+                        review.text +
+                        '</div>' +
+                        '<div class="row justify-content-between mb-0">' +
+                        '<div class="col-auto mt-1 ml-2 row text-nowrap">' +
+                        '<x class="defaultCursor">' +
+                        review.positiveReactions +
+                        '</x>' +
+                        '<i class="fa fa-thumbs-up px-1 pt-1 text-secondary pointer" id="negativeReaction_' + review.id +
+                        '" onmouseover="blueThis(this.id);" onmouseout="blueThis(this.id);" onclick="likeReview(' + user_id + ', ' + place_id + ', ' + review.id + ');"></i>' +
+                        '<y class="defaultCursor">' +
+                        review.negativeReactions +
+                        '</y>' +
+                        '<i class="fa fa-thumbs-down pl-1 pt-1 text-secondary pointer" id="positiveReaction_' + review.id +
+                        '" onmouseover="blueThis(this.id);" onmouseout="blueThis(this.id);" onclick="dislikeReview(' + user_id + ', ' + place_id + ', ' + review.id + ');"></i>' +
+                        '</div>' +
+                        '<div class="col-auto">' +
+                        '<p class="text-nowrap m-0 pt-1 float-right" style="font-size: smaller">' +
+                        'Review added ' + review_time +
+                        '</p>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>'
+                }
             });
             $( "#userReviews" ).html( content );
         });
