695b43463595fa183f0b87c3df2511065f07bec7
vili nguyen <vili2002@centrum.cz>
2020-05-21 15:17:56 +0200

    fixing best user review

--- app/controllers/users.py
+++ app/controllers/users.py
@@ -10,11 +10,12 @@ from .auth import authorized, get_user
 def profile(request, id):
     user = json.loads(requests.get('http://77.244.251.110/api/users/' + id).text)
     reviews = json.loads(requests.get('http://77.244.251.110/api/users/' + id + '/reviews').text)
-    best = reviews[0]
-    for review in reviews:
-        if review['positiveReactions'] > best['positiveReactions']:
-            best = review
-    print(best)
+    best = ""
+    if reviews:
+        best = reviews[0]
+        for review in reviews:
+            if review['positiveReactions'] > best['positiveReactions']:
+                best = review
     return render(request, 'users/index.html',
                   {
                       'user': user,
