1bca5313af78180caf2febaf42e9688a530440c0
Skrymer123 <vojtechtlamka@seznam.cz>
2020-05-23 02:18:22 +0200

    funkční placetype filter

--- app/static/js/places/filters.js
+++ app/static/js/places/filters.js
@@ -1,5 +1,5 @@
 var currentUrl = window.location.href.toLowerCase();
-var filters = ['country=', 'city=', 'isverified=', 'isopen=', 'minrating=', 'maxrating='/*, 'placetype='*/];
+var filters = ['country=', 'city=', 'isverified=', 'isopen=', 'minrating=', 'maxrating='];
 var elementValue = "";
 
 /* NAČÍTÁ AKTUÁLNÍ FILTRY DO HTML INPUTŮ */
@@ -34,12 +34,30 @@ var elementValue = "";
                     else if (i == 5 && elementValue != 5) document.getElementById('filters-MaxRating').value = elementValue;
                 }
             }
+
+        /* PLACETYPES */
+            var tempElement = "";
+            if(currentUrl.includes('placetype=[')) {
+                currentUrl.slice(currentUrl.indexOf('placetype=[') + "placetype=[".length).slice(0, currentUrl.slice(currentUrl.indexOf('placetype=[') + "placetype=[".length).indexOf(']')).split(',').forEach(id => {
+                    window.alert(id);
+                    tempElement = document.getElementById(id);
+                    if(tempElement.classList.contains('placeType')) {
+                        tempElement.checked = (tempElement.id == id);
+                    }
+                })
+            }
+            else {
+                document.getElementById("any").checked = true;
+            }
     })
 
 /* VLASTNÍ FILTERING (mění URL podle vybraných filtrů) */
     function filter() {
         var ordering = "";
         var filtersUrl = "";
+        var placeTypeFilter = "";
+        var counter = 0;
+        var anyPlaceTypeChecked = false;
         var orderingButtonText = document.getElementById('orderingButton').innerText;
         var filterValues = ["", "", "", "", "", ""];
                 /* indexes:   [0]   country (string)
@@ -53,19 +71,30 @@ var elementValue = "";
         /* získá jednotlivé vstupy filtrů */
             filterValues[0] = document.getElementById('filters-Country').value.toLowerCase();
             filterValues[1] = document.getElementById('filters-City').value.toLowerCase();
+
             if(document.getElementById('filters-Verified').checked) {
                 filterValues[2] = "true";
             }
             if(document.getElementById('filters-OpenedState').checked) {
                 filterValues[3] = "true";
             }
-            if(document.getElementById('filters-MinRating').value != 0) {
-                filterValues[4] = document.getElementById('filters-MinRating').value;
-            }
-            if(document.getElementById('filters-MaxRating').value != 5) {
-                filterValues[5] = document.getElementById('filters-MaxRating').value;
-            }
-            /* PLACETYPES */
+
+            filterValues[4] = document.getElementById('filters-MinRating').value;
+            filterValues[5] = document.getElementById('filters-MaxRating').value;
+
+            document.querySelectorAll('.placeType').forEach(element => {
+                if(element.id == "any") {
+                    anyPlaceTypeChecked = true;
+                }
+                else if (counter == 0 && element.checked) {
+                    placeTypeFilter = element.id.toString();
+                    counter++;
+                }
+                else if (element.checked) {
+                    placeTypeFilter = placeTypeFilter + ',' + element.id.toString();
+                }
+            })
+            if(anyPlaceTypeChecked) placeTypeFilter = "";
 
         /* VYPLIVNE ERROR POKUD JE MIN-RATING VĚTŠÍ NEŽ MAX-RATING */
             if(filterValues[4] > filterValues[5]) {
@@ -90,8 +119,15 @@ var elementValue = "";
             filterValues.forEach(function (filterValue, i) {
                 if(filterValue)
                 {
-                    filtersUrl += '&' + filters[i] + filterValue;
+                    if((i == 4 && filterValue == 0) || (i == 5 && filterValue == 5)) {}
+                    else {
+                        filtersUrl += '&' + filters[i] + filterValue;
+                    }
                 }
             })
+            if(placeTypeFilter) {
+                filtersUrl += ('&placetype=[' + placeTypeFilter + ']');
+            }
+
             window.location = '/places/?page=1' + ordering + filtersUrl;
     }
