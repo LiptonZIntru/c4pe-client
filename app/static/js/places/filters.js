2c68a8c5e3fea4997fd7222353ea8b44c9432b96
Skrymer123 <vojtechtlamka@seznam.cz>
2020-05-23 20:49:57 +0200

    filtry podle vyhledávání jména

--- app/static/js/places/filters.js
+++ app/static/js/places/filters.js
@@ -1,18 +1,30 @@
 var currentUrl = window.location.href.toLowerCase();
-var filters = ['country=', 'city=', 'isverified=', 'isopen=', 'minrating=', 'maxrating='];
+var filters = ['name=', 'country=', 'city=', 'isverified=', 'isopen=', 'minrating=', 'maxrating='];
 var elementValue = "";
+
+/* PŘIDÁ DO URL PAGINATION, POKUD TAM CHYBÍ */
+    $(document).ready(function () {
+        if(!currentUrl.includes('page=')) {
+            currentUrl = currentUrl.slice(currentUrl.indexOf('/places/') + '/places/'.length);
+            if(currentUrl.startsWith('?')) {
+                currentUrl = currentUrl.replace('?', '&');
+            }
+            window.location = '?page=1' + currentUrl;
+        }
+    })
 
 /* NAČÍTÁ AKTUÁLNÍ FILTRY DO HTML INPUTŮ */
     $(document).ready(function () {
         /* COUNTRY, CITY */
-            for(var i = 0; i < 2; i++) {
+            for(var i = 0; i < 3; i++) {
                 if (currentUrl.includes(filters[i])) {
                     elementValue = currentUrl.slice(currentUrl.indexOf(filters[i]) + (filters[i].length));
                     if (elementValue.indexOf('&') != -1) {
                         elementValue = elementValue.slice(0, elementValue.indexOf('&'));
                     }
-                    if (i == 0) document.getElementById('filters-Country').value = elementValue;
-                    else document.getElementById('filters-City').value = elementValue;
+                    if (i == 1) document.getElementById('filters-Country').value = elementValue;
+                    else if (i == 2) document.getElementById('filters-City').value = elementValue;
+                    else document.getElementById('searchField').value = elementValue;
                 }
             }
 
@@ -27,11 +39,11 @@ var elementValue = "";
             }
 
         /* MIN a MAX RATING */
-            for(var i = 4; i < 6; i++) {
+            for(var i = 5; i < 7; i++) {
                 if (currentUrl.includes(filters[i])) {
                     elementValue = currentUrl.slice(currentUrl.indexOf(filters[i]) + (filters[i].length), currentUrl.indexOf(filters[i]) + (filters[i].length) + 1);
-                    if (i == 4 && elementValue != 0) document.getElementById('filters-MinRating').value = elementValue;
-                    else if (i == 5 && elementValue != 5) document.getElementById('filters-MaxRating').value = elementValue;
+                    if (i == 5 && elementValue != 0) document.getElementById('filters-MinRating').value = elementValue;
+                    else if (i == 6 && elementValue != 5) document.getElementById('filters-MaxRating').value = elementValue;
                 }
             }
 
@@ -58,28 +70,30 @@ var elementValue = "";
         var counter = 0;
         var anyPlaceTypeChecked = false;
         var orderingButtonText = document.getElementById('orderingButton').innerText;
-        var filterValues = ["", "", "", "", "", ""];
-                /* indexes:   [0]   country (string)
-                              [1]   city (string)
-                              [2]   isVerified ("true"/"")
-                              [3]   isOpen ("true"/"")
-                              [4]   minRating ("1" - "5")
-                              [5]   maxRating ("0" - "4")
+        var filterValues = ["", "", "", "", "", "", ""];
+                /* indexes:   [0]   name (string)
+                              [1]   country (string)
+                              [2]   city (string)
+                              [3]   isVerified ("true"/"")
+                              [4]   isOpen ("true"/"")
+                              [5]   minRating ("1" - "5")
+                              [6]   maxRating ("0" - "4")
                  */
 
         /* získá jednotlivé vstupy filtrů */
-            filterValues[0] = document.getElementById('filters-Country').value.toLowerCase();
-            filterValues[1] = document.getElementById('filters-City').value.toLowerCase();
+            filterValues[0] = document.getElementById('searchField').value.toLowerCase();
+            filterValues[1] = document.getElementById('filters-Country').value.toLowerCase();
+            filterValues[2] = document.getElementById('filters-City').value.toLowerCase();
 
             if(document.getElementById('filters-Verified').checked) {
-                filterValues[2] = "true";
-            }
-            if(document.getElementById('filters-OpenedState').checked) {
                 filterValues[3] = "true";
             }
+            if(document.getElementById('filters-OpenedState').checked) {
+                filterValues[4] = "true";
+            }
 
-            filterValues[4] = document.getElementById('filters-MinRating').value;
-            filterValues[5] = document.getElementById('filters-MaxRating').value;
+            filterValues[5] = document.getElementById('filters-MinRating').value;
+            filterValues[6] = document.getElementById('filters-MaxRating').value;
 
             document.querySelectorAll('.placeType').forEach(element => {
                 if(element.id == "any" && element.checked) {
@@ -95,10 +109,16 @@ var elementValue = "";
             })
             if(anyPlaceTypeChecked) placeTypeFilter = "";
 
-        /* VYPLIVNE ERROR POKUD JE MIN-RATING VĚTŠÍ NEŽ MAX-RATING */
-            if(filterValues[4] > filterValues[5]) {
-                return window.alert("Maximum rating is higher than minimum rating!");
-            }
+        /* ERORRY */
+            /* minrating > maxrating */
+                if(filterValues[5] > filterValues[6]) {
+                    return window.alert("Maximum rating is higher than minimum rating!");
+                }
+
+            /* string input obsahuje '&' */
+                for (var i = 0; i < 3; i++) {
+                    if(filterValues[i].includes('&')) return window.alert("Character '&' is not allowed to filter by.");
+                }
 
         /* zjistí aktuální typ řazení */
             if(currentUrl.includes('orderby='))
@@ -118,7 +138,7 @@ var elementValue = "";
             filterValues.forEach(function (filterValue, i) {
                 if(filterValue)
                 {
-                    if((i == 4 && filterValue == 0) || (i == 5 && filterValue == 5)) {}
+                    if((i == 5 && filterValue == 0) || (i == 6 && filterValue == 5)) {}
                     else {
                         filtersUrl += '&' + filters[i] + filterValue;
                     }
