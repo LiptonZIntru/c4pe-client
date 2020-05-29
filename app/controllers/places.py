b28a62070143d044f14885c3ae714d7ae422e924
vili nguyen <vili2002@centrum.cz>
2020-05-30 01:56:31 +0200

    bug 44 solved

--- app/controllers/places.py
+++ app/controllers/places.py
@@ -25,6 +25,9 @@ def index(request):
 
         url = get_url(request, url)
 
+        frontend_url = get_frontend_url(request)
+        print(frontend_url)
+
         response = requests.get(url)
         types = json.loads(requests.get(settings.API_IP + '/api/placetypes').text)
         try:
@@ -43,7 +46,8 @@ def index(request):
                           'search': request.GET.get('name'),
                           'places': places,
                           'pages': pages,
-                          'types': types
+                          'types': types,
+                          'url': frontend_url
                       })
 
 
