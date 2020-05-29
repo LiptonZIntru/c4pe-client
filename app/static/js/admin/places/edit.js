96102b979f7d09301cdd9cb9abb23c2c322252f1
Skrymer123 <vojtechtlamka@seznam.cz>
2020-05-29 21:55:15 +0200

    update fotky podniku na admin pages

--- app/static/js/admin/places/edit.js
+++ app/static/js/admin/places/edit.js
@@ -1,12 +1,12 @@
 function deleteThis(place_id, string) {
-    if (string != 'avatar') {
+    if (string == 'place') {
         if(confirm('Do you really want to delete this place?')) {
             window.location = '/admin/places/' + place_id + '/delete/';
         }
     }
     else {
         if(confirm('Do you really want to delete this image?')) {
-            window.location = '/admin/places/' + place_id + '/images/' + image_id + '/delete/';
+            window.location = '/admin/places/' + place_id + '/images/' + string + '/delete/';
         }
     }
 }
