b9d8880b6e6efb5b799ce9909c06257ee73711e3
Skrymer123 <vojtechtlamka@seznam.cz>
2020-06-03 16:37:36 +0200

    "BUG" 26 - filtrovani a vyhledavani admin tabulek

--- app/static/js/admin/places/index.js
+++ app/static/js/admin/places/index.js
@@ -1,20 +1,35 @@
-var delete_click = false;
-$(function () {
-    $('.delete').click(function () {
-        delete_click = true;
-        var response = confirm('Do you really want to delete this place?');
-        if(response == true)
-        {
-            var id = $(this).parent().parent().attr('id');
-            window.location = '/admin/places/' + id + '/delete';
-        }
+/* Inicializace filtru a razeni tabulky */
+    $(document).ready(function() {
+        $('.table').DataTable( {
+            ordering: true,
+            searching: true,
+            paging: true,
+            pageLength: 25,
+            "columns": [ null,
+                         null,
+                         { "orderable": false } ]
+        } );
+    } );
+
+
+/* table controls */
+    var delete_click = false;
+    $(function () {
+        $('.delete').click(function () {
+            delete_click = true;
+            var response = confirm('Do you really want to delete this place?');
+            if(response == true)
+            {
+                var id = $(this).parent().parent().attr('id');
+                window.location = '/admin/places/' + id + '/delete';
+            }
+        });
+        $('.edit').click(function () {
+            if(!delete_click)
+            {
+                var id = $(this).attr('id');
+                window.location = '/admin/places/' + id + '/edit/';
+            }
+            delete_click = false;
+        });
     });
-    $('.edit').click(function () {
-        if(!delete_click)
-        {
-            var id = $(this).attr('id');
-            window.location = '/admin/places/' + id + '/edit/';
-        }
-        delete_click = false;
-    });
-});
--- app/static/js/admin/users/index.js
+++ app/static/js/admin/users/index.js
@@ -1,25 +1,42 @@
-var delete_click = false;
-$(function () {
-    $('.delete').click(function () {
-        delete_click = true;
-        var response = confirm('Do you really want to delete this user?');
-        if(response == true)
-        {
+/* Inicializace filtru a razeni tabulky */
+    $(document).ready(function() {
+        $('.table').DataTable( {
+            ordering: true,
+            searching: true,
+            paging: true,
+            pageLength: 25,
+            "columns": [ null,
+                         null,
+                         null,
+                         null,
+                         { "orderable": false } ]
+        } );
+    } );
+
+
+/* table controls */
+    var delete_click = false;
+    $(function () {
+        $('.delete').click(function () {
+            delete_click = true;
+            var response = confirm('Do you really want to delete this user?');
+            if(response == true)
+            {
+                var id = $(this).parent().parent().attr('id');
+                window.location = '/admin/users/' + id + '/delete';
+            }
+        });
+        $('.reviews').click(function () {
+            delete_click = true;
             var id = $(this).parent().parent().attr('id');
-            window.location = '/admin/users/' + id + '/delete';
-        }
+            window.location = '/admin/users/' + id + '/reviews';
+        });
+        $('.edit').click(function () {
+            if(!delete_click)
+            {
+                var id = $(this).attr('id');
+                window.location = '/admin/users/' + id + '/edit';
+            }
+            delete_click = false;
+        })
     });
-    $('.reviews').click(function () {
-        delete_click = true;
-        var id = $(this).parent().parent().attr('id');
-        window.location = '/admin/users/' + id + '/reviews';
-    });
-    $('.edit').click(function () {
-        if(!delete_click)
-        {
-            var id = $(this).attr('id');
-            window.location = '/admin/users/' + id + '/edit';
-        }
-        delete_click = false;
-    })
-});
